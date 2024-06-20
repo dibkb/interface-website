15641fc84273ebc518343dca7462e99646e3080b
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2024-06-20 16:30:35 +0530

    Image grayscale removed

--- app/_components/MarqueeComponent.tsx
+++ app/_components/MarqueeComponent.tsx
@@ -9,7 +9,7 @@ const MarqueeComponent = ({image, text} : MarqueeComponentProp) => {
     return (
         <div className="w-[293px] border border-dashed border-neutral-400 h-fit relative mx-2">
             <div className='w-full'>
-                <Image className="h-[218px] w-full border-b border-dashed border-neutral-400 grayscale" src={image} alt='image' quality={100} />
+                <Image className="h-[218px] w-full border-b border-dashed border-neutral-400" src={image} alt='image' quality={100} />
             </div>
             <div className="text-stone-900 text-base font-semibold leading-tight text-center p-5">
                 {text}
