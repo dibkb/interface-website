67c7f25f3e97274dc6049f6cd91e34ca1b664451
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2024-07-03 16:07:09 +0530

    target blank

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -33,12 +33,12 @@ const Navbar = () => {
                                 faq
                             </button>
                         </Link>
-                        <Link href='https://theinterfacecompany.notion.site/Interface-AI-Hiring-68fca33b53d6421dbd9b706f54fa035a?pvs=4'>
+                        <Link target="_blank" href='https://theinterfacecompany.notion.site/Interface-AI-Hiring-68fca33b53d6421dbd9b706f54fa035a?pvs=4'>
                             <button className={cn("font-semibold leading-tight transition-all ease-in-out", mode === "light" ? "text-zinc-900" : "text-white")}>
                                 join us
                             </button>
                         </Link>
-                        <Link href='https://calendly.com/rebhav-getinterface/30min'>
+                        <Link target="_blank" href='https://calendly.com/rebhav-getinterface/30min'>
                             <button className={cn("font-semibold leading-tight transition-all ease-in-out", mode === "light" ? "text-zinc-900" : "text-white")}>
                                 contact us
                             </button>
@@ -69,12 +69,12 @@ const Navbar = () => {
                             faq
                         </button>
                     </div>
-                    <Link href='https://theinterfacecompany.notion.site/Interface-AI-Hiring-68fca33b53d6421dbd9b706f54fa035a?pvs=4'>
+                    <Link target="_blank" href='https://theinterfacecompany.notion.site/Interface-AI-Hiring-68fca33b53d6421dbd9b706f54fa035a?pvs=4'>
                         <button className={cn("font-semibold leading-tight transition-all ease-in-out", mode === "light" ? "text-zinc-900" : "text-white")}>
                             join us
                         </button>
                     </Link>
-                    <Link href='https://calendly.com/rebhav-getinterface/30min'>
+                    <Link target="_blank" href='https://calendly.com/rebhav-getinterface/30min'>
                         <button className={cn("font-semibold leading-tight transition-all ease-in-out", mode === "light" ? "text-zinc-900" : "text-white")}>
                             contact us
                         </button>
