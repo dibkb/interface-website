0555cdbd2fb30e4f2aa53f0fd12e8c63c6a68233
kausthub-kannan <kausthubkannan961@gmail.com>
2024-10-24 23:32:14 +0530

    fix: remove timeleft bar

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -48,34 +48,6 @@ const Navbar = () => {
     <>
       <nav className={"w-full fixed top-0 z-[999] border-b border-neutral-200"}>
         <AnimatePresence>
-          {visible && (
-            <motion.div
-              exit={{ opacity: 0, y: -10 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ ease: "easeInOut", duration: 0.1 }}
-              className="w-full py-2 text-xs lg:text-sm flex justify-between px-5 lg:px-5 items-center bg-primary-green font-medium"
-            >
-              <div className="block"></div>
-              <Link
-                href={"https://cal.com/rebhav-bharadwaj/introductory-chat"}
-                target="_blank"
-              >
-                <div className="text-center text-xs lg:text-lg inline-flex items-center gap-2 px-4">
-                  ⚠️ {timeLeft} : Time left to claim last 18 months of refunds
-                  before new reimbursement policy
-                  {/* <ArrowLineUpRight /> */}
-                </div>
-              </Link>
-              <div
-                role="button"
-                onClick={() => {
-                  setVisible(false);
-                }}
-              >
-                <X size={20} />
-              </div>
-            </motion.div>
-          )}
         </AnimatePresence>
         <div
           className={cn(
